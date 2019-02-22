8b33aaf7e9f95969729df16ba1aaa7602bb69bac
Mark Westoby <mark.westoby@mq.edu.au>
2019-02-22 11:30:38 +1100

    improved fig

--- scripts/workshop.R
+++ scripts/workshop.R
@@ -2,4 +2,18 @@
 ## The csv file containing the data is in the 'data/' directory.
 ## If you want to save plots you created, place them in the 'figures/' directory.
 ## You can create additional R files in the 'scripts/' directory.
+library("ggplot2")
 
+library(tidyverse)
+install.packages("tidyverse")
+# Load the data provided; using read_csv from tidyverse
+# ctrl-enter to run
+interviews_plotting <- read_csv("data_output/interviews_plotting.csv")
+#following formulation creates object plot1, which can then be added to
+plot1 <- ggplot(data=interviews_plotting,
+       aes(x=no_membrs,y=number_items)) 
+# plus needs to be on same line, indicates more is coming
+plot1 +
+  geom_jitter(alpha=0.5, width = 0.1, aes(colour=village))
+ggsave("fig_output/items_membrs.png", plot1, width=15, height =10, dpi=300)
+#dpi 300 is appropriate for printed publication, 72 good enough for screen
